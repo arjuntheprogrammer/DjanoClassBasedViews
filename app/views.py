8cf6d71b49ff93dcac67c80044a7e98eaaac8d9c
Arjun Gupta <saimzn4241@gmail.com>
2017-11-15 01:31:52 +0530

    context

--- app/views.py
+++ app/views.py
@@ -3,6 +3,19 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.utils.timezone import now 
 
 class AboutUsView(TemplateView):
     template_name = 'about_us.html'
+
+def get_context_data(self, **kwargs):
+    context = super(AboutUsView, self).get_context_data(**kwargs)
+    context['now'] = now().weekday
+    print now().weekday()
+    print now().hour()
+
+    if now().weekday() < 5 and 8< now().hour < 18:
+        context['open'] = True
+    else: 
+        context['open'] = False
+    return context
